e2766602fce79d9e6f490a5c75352e685c15f10f
quantumsheep <nathanael.dmc@outlook.fr>
2020-03-31 11:56:53 +0200

    Fix dependencies

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,8 +1,10 @@
-import * as testRunner from 'vscode/lib/testrunner';
+import * as path from 'path'
+import { runTests } from 'vscode-test'
 
-testRunner.configure({
-    ui: 'tdd',
-    useColors: true
-});
+const extensionDevelopmentPath = path.resolve(__dirname, '../')
+const extensionTestsPath = path.resolve(__dirname, './')
 
-module.exports = testRunner;
+runTests({
+  extensionDevelopmentPath,
+  extensionTestsPath,
+})
